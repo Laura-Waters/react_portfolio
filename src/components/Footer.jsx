89bfa09e4672b5446cc262a1fec3a7d501ec5a2e
Laura <laurawatersalston@gmail.com>
2024-08-01 08:30:42 -0400

    adds header and footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className='d-flex flex-wrap align-items-center mx-4'>
+        <footer className='d-flex flex-wrap align-items-center'>
             <ul className='nav col-md-4 justify-content-center list-unstyled d-flex'>
                 <li className='ms-3'><a className='text-muted' href='https://github.com/Laura-Waters'>GitHub</a></li>
                 <li className='ms-3'><a className='text-muted' href='https://www.linkedin.com/in/laura-alston-778241145/'>LinkedIn</a></li>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ function Navigation() {
       <nav className="navbar navbar-expand-lg navbar-light">
         <a><Link to="/"><h1 style={{color: 'black'}}>Laura Alston</h1></Link></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"></button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul className="navbar-nav d-flex flex-end" style={{ display: 'flex', gap: '10px' }}>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/portfolio">Portfolio</Link></li>
