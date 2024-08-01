a82daf9fd55152f2b0960ea9b047ed865d6d2d41
Laura <laurawatersalston@gmail.com>
2024-08-01 09:57:09 -0400

    contact form

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,10 +7,10 @@ function Navigation() {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"></button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul className="navbar-nav d-flex flex-end" style={{ display: 'flex', gap: '10px' }}>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/portfolio">Portfolio</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/resume">Resume</Link></li>
+            <li><Link to="/about" activeClassName="active">About</Link></li>
+            <li><Link to="/portfolio" activeClassName="active">Portfolio</Link></li>
+            <li><Link to="/contact" activeClassName="active">Contact</Link></li>
+            <li><Link to="/resume" activeClassName="active">Resume</Link></li>
         </ul>
         </div>
       </nav>
