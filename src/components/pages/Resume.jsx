5ac595f4c981ddcd23ae012aec295330b7fa39ec
Laura <laurawatersalston@gmail.com>
2024-08-01 12:35:39 -0400

    resume

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,10 +3,28 @@ export default function Resume() {
       <div>
         <h2>Resume</h2>
         <p>
-          See my full resume <a href='../docs/LauraAlston_Resume_2024.pdf' style={{ textDecoration: 'underline' }}>here</a>.
+          See my full resume <a href='../docs/LauraAlston_Resume_2024.pdf' id='here' style={{ textDecoration: 'underline' }}>here</a>.
         </p>
-        <h3>Front-End Proficiencies</h3>
-        <h3>Back-End Proficiencies</h3>
+        <h2>Front-End Proficiencies</h2>
+        <ul className="bulletedlist">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>Responsive Design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>Bulma</li>
+        </ul>
+        <h2>Back-End Proficiencies</h2>
+        <ul className="bulletedlist">
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>JQuery</li>
+          <li>My SQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+        </ul>
       </div>
     );
 }
