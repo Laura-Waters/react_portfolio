25e65320bb864cd242f68b2f555eed90fd56d431
Laura <laurawatersalston@gmail.com>
2024-08-01 08:49:24 -0400

    adds hero image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,11 @@ function Header() {
     return (
         <div className='mx4' style={{ backgroundColor: 'lightBlue' }}>
             <Navigation />
+            <section className="hero"><span role="img" aria-label="A blurred view of city lights."></span>
+                <div className="subtitle-block">
+                    <p className="subtitle">Web Developer & Designer</p>
+                </div>
+            </section>
         </div>
     );
 }
